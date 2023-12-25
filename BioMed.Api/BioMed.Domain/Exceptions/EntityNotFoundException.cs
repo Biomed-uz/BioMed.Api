42ae0107380d7e87026d4a113ccc529a32dfffa1
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2023-12-25 20:44:30 +0500

    Added Exception in the BioMed.Domain

---
+++ BioMed.Api/BioMed.Domain/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioMed.Domain.Exceptions
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException()
+        {
+        }
+
+        public EntityNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public EntityNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
