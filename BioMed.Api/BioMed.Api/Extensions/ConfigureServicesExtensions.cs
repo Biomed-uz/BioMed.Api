cc86800662cdc8f3d6037b3dde70c7d206a8b720
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-08 22:29:44 +0500

    Added ConfigureRepositories extension method

--- BioMed.Api/BioMed.Api/Extensions/ConfigureServicesExtensions.cs
+++ BioMed.Api/BioMed.Api/Extensions/ConfigureServicesExtensions.cs
@@ -1,7 +1,27 @@
-﻿namespace BioMed.Api.Extensions
-{
-    public class ConfigureServicesExtensions
-    {
+﻿using BioMed.Domain.Interfaces.Repositories;
+using BioMed.Infrastructure.Persistence.Repositories;
 
+namespace BioMed.Api.Extensions
+{
+    public static class ConfigureServicesExtensions
+    {
+        public static IServiceCollection ConfigureRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IPatientRepository, PatientRepository>();
+            services.AddScoped<IDoctorRepository, DoctorRepository>();
+            services.AddScoped<IDoctorCategoryRepository, DoctorCategoryRepository>();
+            services.AddScoped<IDoctorDepartmentRepository, DoctorDepartmentRepository>();
+            services.AddScoped<IVisitRepostory, VisitRepository>();
+            services.AddScoped<IPaymentRepository, PaymentRepository>();
+            services.AddScoped<ITestTypeRepository, TestTypeRepository>();
+            services.AddScoped<ILabResultRepository, LabResultRepository>();
+            services.AddScoped<IDiseaseCategoryRepository, IDiseaseCategoryRepository>();
+            services.AddScoped<IDiseaseRepository, IDiseaseRepository>();
+            services.AddScoped<ITreatmentRepository, TreatmentRepository>();
+
+            return services;
+        }
     }
 }
