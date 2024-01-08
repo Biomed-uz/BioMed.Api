c6cca939d76e0f9665212974e7d87efa0c22f80d
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-08 22:55:21 +0500

    Added ICommonRepository

---
+++ BioMed.Api/BioMed.Domain/Interfaces/Repositories/ICommonRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace BioMed.Domain.Interfaces.Repositories
+{
+    public interface ICommonRepository
+    {
+        public ICategoryRepository Category { get; }
+        public IDepartmentRepository Department { get; }
+        public IDoctorRepository Doctor { get; }
+        public IDoctorCategoryRepository DoctorCategory { get; }
+        public IDoctorDepartmentRepository DoctorDepartment { get; }
+        public IPatientRepository Patient { get; }
+        public IVisitRepostory Visit {  get; }
+        public IPaymentRepository Payment { get; }
+        public ITestTypeRepository TestType { get; }
+        public ILabResultRepository LabResult { get; }
+        public IDiseaseCategoryRepository DiseaseCategory { get; }
+        public IDiseaseRepository Disease { get; }
+        public ITreatmentRepository Treatment { get; }
+
+        public int SaveChanges();
+    }
+}
