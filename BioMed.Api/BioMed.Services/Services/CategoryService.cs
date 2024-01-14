dbd2b8b6951d65d90144d956583dc8ce3492987b
Zikirillo2002 <128654271+Zikirillo2002@users.noreply.github.com>
2024-01-14 11:44:47 +0500

    Created Category service

---
+++ BioMed.Api/BioMed.Services/Services/CategoryService.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using BioMed.Domain.DTOs.Category;
+using BioMed.Domain.Entities;
+using BioMed.Domain.Interfaces.Services;
+using BioMed.Domain.Pagination;
+using BioMed.Domain.ResourceParameters;
+using BioMed.Infrastructure.Persistence;
+using Microsoft.Extensions.Logging;
+
+namespace BioMed.Services.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly IMapper _mapper;
+        private readonly BioMedDbContext _context;
+
+        public CategoryService(IMapper mapper,
+            BioMedDbContext context)
+        {
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public PaginatedList<CategoryDto> GetCategories(
+            CategoryResourceParameters categoryResourceParameters)
+        {
+            var query = _context.Categories.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(categoryResourceParameters.SearchString))
+            {
+                query = query.Where(x => x.Name.Contains(categoryResourceParameters.SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(categoryResourceParameters.OrderBy))
+            {
+                query = categoryResourceParameters.OrderBy.ToLowerInvariant() switch
+                {
+                    "name" => query.OrderBy(x => x.Name),
+                    "namedesc" => query.OrderByDescending(x => x.Name),
+                    _ => query.OrderBy(x => x.Name),
+                };
+            }
+            var categories = query.ToPaginatedList(categoryResourceParameters.PageSize,
+                categoryResourceParameters.PageNumber);
+
+            var categoryDtos = _mapper.Map<List<CategoryDto>>(categories);
+
+            return new PaginatedList<CategoryDto>(categoryDtos,
+                categories.TotalCount,
+                categories.CurrentPage,
+                categories.PageSize);
+        }
+
+        public CategoryDto? GetCategoryById(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(x => x.Id == id);
+
+            var categoryDto = _mapper.Map<CategoryDto>(category);
+
+            return categoryDto;
+        }
+
+        public CategoryDto CreateCategory(CategoryForCreateDto categoryToCreate)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryToCreate);
+
+            _context.Categories.Add(categoryEntity);
+            _context.SaveChanges();
+
+            var categoryDto = _mapper.Map<CategoryDto>(categoryEntity);
+
+            return categoryDto;
+        }
+
+        public void UpdateCategory(CategoryForUpdateDto categoryToUpdate)
+        {
+            var categoryEntity = _mapper.Map<Category>(categoryToUpdate);
+
+            _context.Categories.Update(categoryEntity);
+            _context.SaveChanges();
+        }
+
+        public void DeleteCategory(int id)
+        {
+            var category = _context.Categories.FirstOrDefault(x => x.Id == id);
+            if (category is not null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
